030e2922f3f1de9b65132a322a708fd619ac1279
maede <maedeh7676@gmail.com>
2019-12-21 17:39:38 +0330

    headmaster back added

--- university/application/controllers/uni.php
+++ university/application/controllers/uni.php
@@ -49,4 +49,51 @@ class uni extends CI_Controller {
 		
 	}//end chek login
 	
+	
+	function return_master()
+	{
+	$this->load->view('headmaster.php');}
+//	function show_news()
+//	{$this->load->view('food.php');}
+	function heaadmaster_list()
+	{$info['test']=0;
+	$this->load->view('heaadmaster_list',$info);}
+	//ذخیره دروس توسط مدیر گروه
+	
+	
+	
+	function save_course()
+	{
+	//	--------------------------------------------------------------------------------------------------------------
+	$this->load->library('form_validation');
+		//<!-----------------rule------------------>
+		$this->form_validation->set_rules('course_name','نام درس', 'required');
+		$this->form_validation->set_rules('teacher_name','نام استاد','required');
+		$this->form_validation->set_rules('class_time','زمان کلاس','required');
+		$this->form_validation->set_rules('exam_time','زمان امتحان','required');
+		$this->form_validation->set_rules('max','ظرفیت','required');
+		//------------------message-----------------
+		$this->form_validation->set_message('required', 'پربودن فیلد %s اجباری است ');
+		//--------------------checking------------
+		if ($this->form_validation->run() == FALSE)
+		{
+			$info['test']=2;
+	$this->load->view('heaadmaster_list',$info);
+		}
+		else
+	{
+	$this->data_model->save_course($_POST['course_name'],$_POST['teacher_name'],$_POST['class_time'],$_POST['exam_time'],$_POST['max']);
+	$info['test']=1;
+	$this->load->view('heaadmaster_list',$info);
+	}
+	}
+	//نمایش دروس ذخیره شده برای مدیر گروه
+	function show_course_list()
+	{
+		$data['info']=$this->data_model->show_course_list();
+		$this->load->view('show_course_list',$data);
+	}
+	
+	
+}
 
--- university/application/models/data_model.php
+++ university/application/models/data_model.php
@@ -9,4 +9,22 @@ class data_model extends CI_model {
 		$query=$this->db->query($sql);
 		return $query->result_array();	
 	}
+	
+	
+	
+	function save_course($course_name,$teacher_name,$class_time,$exam_time,$max)
+	{
+		$sql='insert into course(course_name,teacher_name,class_time,exam_time,max)
+		values("'.$course_name.'","'.$teacher_name.'","'.$class_time.'","'.$exam_time.'",'.$max.')';
+		$this->db->query($sql);
+		
+	}
+	function show_course_list()
+	{
+		
+		$sql='select * from course';
+    	$query=$this->db->query($sql);
+		return $query->result_array();
+	}
+	
 }
